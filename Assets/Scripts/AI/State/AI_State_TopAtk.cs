25dcea9af21a4c9da7c47fab04f383052c31e482
Guo001 <sss7634h9@gmail.com>
2022-06-18 23:19:30 +0800

    [fix] GameManager 加上 Singleton ，AI改抓GameManager player

--- Assets/Scripts/AI/State/AI_State_TopAtk.cs
+++ Assets/Scripts/AI/State/AI_State_TopAtk.cs
@@ -3,7 +3,6 @@ using DG.Tweening;
 
 public class AI_State_TopAtk : MonoBehaviour, StateInterface
 {
-    public Transform mEnemyPos = null;
     public Transform Pos_TopLeft = null;
     public Transform Pos_TopRight = null;
 
@@ -44,11 +43,11 @@ public class AI_State_TopAtk : MonoBehaviour, StateInterface
     private void TopAtk(Transform iTopPos)
     {
         float aSpeed = Random.Range(mData.mMinTopAtkSpeed, mData.mMaxTopAtkSpeed);
-        Quaternion aRot = Quaternion.FromToRotation(iTopPos.right, (mEnemyPos.position - iTopPos.position));
+        Quaternion aRot = Quaternion.FromToRotation(iTopPos.right, (GameManager.Instance.player.transform.position - iTopPos.position));
         Sequence mTopAtk = DOTween.Sequence();
         mTopAtk.SetAutoKill(false);
         mTopAtk.Append(iTopPos.DOLocalRotateQuaternion(aRot, 0.01f));
-        mTopAtk.Append(iTopPos.DOMove(mEnemyPos.position, aSpeed));
+        mTopAtk.Append(iTopPos.DOMove(GameManager.Instance.player.transform.position, aSpeed));
         mTopAtk.OnComplete(() =>
         {
             mTopAtk.PlayBackwards();
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     [Header("GameObject")]
     public Text TimerText;
