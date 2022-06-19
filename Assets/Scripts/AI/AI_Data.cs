cc28dbad586292f9cae7a7e52361c769472da54f
Guo001 <sss7634h9@gmail.com>
2022-06-19 17:25:36 +0800

    [fix] 反應圖layer 改為-5

--- Assets/Scripts/AI/AI_Data.cs
+++ Assets/Scripts/AI/AI_Data.cs
@@ -51,6 +51,7 @@ public class AI_Data : ScriptableObject
         aEventRender.transform.localPosition = mEventRendererOffset;
         aEventRender.transform.localScale = Vector3.one;
         mEventRenderer = aEventRender.AddComponent<SpriteRenderer>();
+        mEventRenderer.sortingLayerID = -5;
         mEventRenderer.gameObject.SetActive(false);
     }
 
